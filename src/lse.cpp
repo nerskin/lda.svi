3734daf8e714c0fd6e2eef7a54d3421bb485950b
Nicholas Erskine <nicholas.erskine95@gmail.com>
2019-06-21 18:28:54 +1000

    tidy up lse

--- src/lse.cpp
+++ src/lse.cpp
@@ -8,7 +8,6 @@ using namespace std;
 // [[Rcpp::export]]
 double lse(const arma::rowvec &log_probs){
 	double alpha = arma::max(log_probs);
-	cout << alpha << endl;
 	double result = alpha + std::log(arma::accu(arma::exp(log_probs - alpha)));
 	return result;
 }
